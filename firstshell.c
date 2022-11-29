de6e8c88a2822de349fd64e705e08e0a8d5cff76
CyrineZekri <cyrinezekri4@gmail.com>
2022-11-29 11:42:40 +0100

    first attenmpt

---
+++ firstshell.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * first_step
+ * Return:void.
+ */
+void first_step(void)
+{
+    int status = 1;
+  char* readline; 
+
+    while (status)
+    {
+        printf("$");
+        /**wait for user to enter commands */
+
+        if (status = 0)
+        {
+            break;
+        }
+    }
+}
