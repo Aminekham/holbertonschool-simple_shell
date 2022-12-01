714d2f12457eb804280472011e295f9a0848b5be
Aminekham <lkham0508@gmail.com>
2022-12-01 17:28:13 +0100

    update 0.1

--- mainshell.c
+++ mainshell.c
@@ -6,24 +6,18 @@
 
 int main(void)
 {
-    int status = 1;
     size_t l = 0;
     char *readline;
     char **command = malloc(sizeof(char*) * 1024);
     int executed;
 
     printf("$");
-    while (status)
+    while (1)
     {
         /**wait for user to enter commands */
         getline(&readline, &l, stdin);
         command = parse_the_line(readline);
         executed = execute_the_line(command);
-          if (executed != status)
-        {
-            break;
-        }
-        free(readline);
 	    printf("$");
     }
     return(1);
