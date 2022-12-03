79d6f4306310a1727dd59bdc45b70a53b64394a9
Aminekham <lkham0508@gmail.com>
2022-12-03 22:19:28 +0100

    header fixed

--- main.h
+++ main.h
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-void first_step(void);
+#include <sys/wait.h>
+
+int shell(char *readline);
 char *get_the_line(void);
 char **parse_the_line(char *buff);
 int execute_the_line(char * buuf[]);
