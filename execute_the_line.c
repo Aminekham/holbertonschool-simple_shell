51f2009416ff9249bb2f07353e41ffc294808416
Aminekham <lkham0508@gmail.com>
2022-12-04 16:34:21 +0100

    fixing output of task 2

--- execute_the_line.c
+++ execute_the_line.c
@@ -7,14 +7,17 @@
 int execute_the_line(char * buuf[])
 {
 	int v = 0;
-	char c[] = "/bin/";
+	char *env[] = 
+	{
+        "HOME=/",
+        "PATH=/bin:/usr/bin",
+        "TZ=UTC0",
+        "USER=beelzebub",
+        "LOGNAME=tarzan",
+        0
+    };
 
 	printf("the value of v = %d", v);
-	strcat(c, buuf[0]);
-	v = execve(c, buuf, NULL);
-	if (v == -1)
-	{
-		printf("no its here");
-	}
+	v = execve(buuf[0], buuf, env);
 	return (v);
 }
