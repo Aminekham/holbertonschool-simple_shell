7291176e1a5e1a6de7fe1e6dbd3a5c902321a0df
Aminekham <lkham0508@gmail.com>
2022-12-07 21:46:00 +0100

    fixed

--- execute_the_line.c
+++ execute_the_line.c
@@ -8,25 +8,10 @@ int execute_the_line(char * buuf[])
 {
 	int v = 0;
 	char c[] = "/bin/";
-	char *env[] = {
-        "HOME=/desktop",
-        "PATH=/bin:/usr/bin",
-        "TZ=UTC0",
-        "USER=root",
-        "LOGNAME=root",
-        0
-    };
 	char *b = "/";
 	
-	if (*buuf[0] != *b)
-	{
-		strcat(c, buuf[0]);
-		v = execve(c, buuf, env);
-	}
-	else
-	{
-		v = execve(buuf[0], buuf, env);
-	}
+	strcat(c, buuf[0]);
+	v = execve(c, buuf, NULL);
 	if (v == -1)
 	{
 		printf("command doesn't exist\n");
