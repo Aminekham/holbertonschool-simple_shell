b213ed55835321ae391bd436a0d5a77076405bd6
Aminekham <lkham0508@gmail.com>
2022-12-08 16:46:30 +0100

    getting the line function

--- get_the_line.c
+++ get_the_line.c
@@ -3,7 +3,6 @@
  * 
  * 
 */
-extern char **environ;
 char *get_the_line(void)
 {
 	int k;
@@ -11,31 +10,19 @@ char *get_the_line(void)
 	char *userinput;
 
 	k = getline(&userinput, &l, stdin);
-	if (k == EOF)
+	if (userinput == NULL)
+	{
+		perror("A problem while mallocing the buffer");
+	}
+	else if (k == EOF)
 	{
 		free(userinput);
 		exit(0);
 	}
-	if (strcmp(userinput, "env\n") == 0)
-	{
-		char **env = environ;
-
-		while (*env)
-		{
-			printf("%s\n", *env);
-			env++;
-		}
-		free(userinput);
-		return NULL;
-	}
-	if (strcmp(userinput, "exit\n") == 0)
+	else if (strcmp(userinput, "exit\n") == 0)
 	{
 		free(userinput);
-		exit(98);
-	}
-	if (userinput == NULL)
-	{
-		perror("A problem while mallocing the buffer");
+		exit(0);
 	}
 	return (userinput);
 }
