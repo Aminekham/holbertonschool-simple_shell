112bd2e6cd617a18192644df3f2bdf04b2a2a2b5
Aminekham <lkham0508@gmail.com>
2022-12-08 11:08:54 +0100

    fixing 2

--- execute_the_line.c
+++ execute_the_line.c
@@ -11,6 +11,11 @@ int execute_the_line(char * buuf[])
 	v = execve(buuf[0], buuf, NULL);
 	if (v == -1)
 	{
+		while (*buuf != NULL)
+		{
+			free(*buuf);
+			buuf++;
+		}
 		printf("command doesn't exist\n");
 		exit(98);
 	}
