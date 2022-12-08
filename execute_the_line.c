42b120f9e389ae7b922b69f8abbb8552d84f8a13
Aminekham <lkham0508@gmail.com>
2022-12-09 00:48:42 +0100

    fixing 0.6

--- execute_the_line.c
+++ execute_the_line.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *execute_the_line - function to excute the line
- *@buuf: the whole command parsed
- *Return: returns an int value
+ *
+ *
+ *
  */
-int execute_the_line(char *buuf[])
+int execute_the_line(char * buuf[])
 {
 	int v = 0;
 	char c[] = "/bin/";
@@ -13,32 +13,13 @@ int execute_the_line(char *buuf[])
 
 	if (f == k)
 	{
-		v = execve(buuf[0], buuf, NULL);
-		if (v == -1)
-		{
-			while (*buuf)
-			{
-				free(*buuf);
-				buuf++;
-			}
-			free(buuf);
-			return (v);
-		}
+		execve(buuf[0], buuf, NULL);
 	}
-	else
+	strcat(c, buuf[0]);
+	v = execve(c, buuf, NULL);
+	if (v == -1)
 	{
-		strcat(c, buuf[0]);
-		v = execve(c, buuf, NULL);
-		if (v == -1)
-		{
-			while (*buuf)
-			{
-				free(*buuf);
-				buuf++;
-			}
-			return (v);
-			free(buuf);
-		}
+		return(v);
 	}
 	return (v);
 }
