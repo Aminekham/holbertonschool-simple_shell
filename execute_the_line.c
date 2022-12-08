b5b300e1294f9b61269f6bdb2085fbf1b984c97b
Aminekham <lkham0508@gmail.com>
2022-12-09 00:03:59 +0100

    fixing 0.6

--- execute_the_line.c
+++ execute_the_line.c
@@ -8,34 +8,12 @@ int execute_the_line(char * buuf[])
 {
 	int v = 0;
 	char c[] = "/bin/";
-	char k = '/';
-	char f = buuf[0][0];
 
-	if (f == k)
-	{
-		v = execve(buuf[0], buuf, NULL);
-		if (v == -1)
-	{
-		while (*buuf)
-		{
-			free(*buuf);
-			buuf++;
-		}
-		free(buuf);
-		return(v);
-	}
-	}
 	strcat(c, buuf[0]);
 	v = execve(c, buuf, NULL);
 	if (v == -1)
 	{
-		while (*buuf)
-		{
-			free(*buuf);
-			buuf++;
-		}
-		free(buuf);
-		return(v);
+		printf("no its here");
 	}
 	return (v);
 }
