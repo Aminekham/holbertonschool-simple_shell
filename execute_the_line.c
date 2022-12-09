6611d238e1e8c480f99d87823a533f381d5cb992
Aminekham <lkham0508@gmail.com>
2022-12-09 10:08:21 +0100

    fixing 0.7

--- execute_the_line.c
+++ execute_the_line.c
@@ -8,14 +8,7 @@ int execute_the_line(char *buuf[])
 {
 	int v = 0;
 	char c[] = "/bin/";
-	char k = '/';
-	char f = buuf[0][0];
 
-	if (f == k)
-	{
-		v = execve(buuf[0], buuf, NULL);
-		return(v);
-	}
 	strcat(c, buuf[0]);
 	v = execve(c, buuf, NULL);
 	if (v == -1)
