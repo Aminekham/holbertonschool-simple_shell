6245b35bb975c72fb39d6b36c80e2fa455586e7b
Aminekham <lkham0508@gmail.com>
2022-12-09 12:01:22 +0100

    exec

--- execute_the_line.c
+++ execute_the_line.c
@@ -7,11 +7,19 @@
 int execute_the_line(char *buuf[])
 {
 	int v = 0;
+	char c[] = "/bin/";
+	char k = '/';
+	char f = buuf[0][0];
 
-	v = execve(buuf[0], buuf, NULL);
+	if (f == k)
+	{
+		execve(buuf[0], buuf, NULL);
+	}
+	strcat(c, buuf[0]);
+	v = execve(c, buuf, NULL);
 	if (v == -1)
 	{
-		return (v);
+		printf("no its here");
 	}
 	return (v);
 }
