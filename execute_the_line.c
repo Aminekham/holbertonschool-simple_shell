8aa5e084cbb5d600778fa1f4741968fecf674db3
Aminekham <lkham0508@gmail.com>
2022-12-09 12:10:33 +0100

    final

--- execute_the_line.c
+++ execute_the_line.c
@@ -4,22 +4,21 @@
  *@buuf: the buffer with the whole command
  *Return: integer of the status
  */
-int execute_the_line(char *buuf[])
+int execute_the_line(char * buuf[])
 {
-	int v = 0;
+	int v = 0, i;
 	char c[] = "/bin/";
-	char k = '/';
-	char f = buuf[0][0];
 
-	if (f == k)
-	{
-		execve(buuf[0], buuf, NULL);
-	}
-	strcat(c, buuf[0]);
-	v = execve(c, buuf, NULL);
+	v = execve(buuf[0], buuf, NULL);
 	if (v == -1)
 	{
-		printf("no its here");
+		while (*buuf != NULL)
+		{
+			free(*buuf);
+			buuf++;
+		}
+		printf("command doesn't exist\n");
+		exit(98);
 	}
 	return (v);
 }
