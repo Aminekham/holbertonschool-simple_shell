fcf3be4ae4f6fe3021cb3e459bba63ad72373a0b
Aminekham <lkham0508@gmail.com>
2022-12-09 11:38:30 +0100

    Fixing execute

--- execute_the_line.c
+++ execute_the_line.c
@@ -4,22 +4,13 @@
  *@buuf: the buffer with the whole command
  *Return: integer of the status
  */
-int execute_the_line(char *buuf[])
+int execute_the_line(char * buuf[])
 {
 	int v = 0;
 	char c[] = "/bin/";
-	char k = '/';
-	char f = buuf[0][0];
 
-	if (f == k)
-	{
-		execve(buuf[0], buuf, NULL);
-	}
-	else
-	{
-		strcat(c, buuf[0]);
-		v = execve(c, buuf, NULL);
-	}
+	strcat(c, buuf[0]);
+	v = execve(c, buuf, NULL);
 	if (v == -1)
 	{
 		printf("command not found");
