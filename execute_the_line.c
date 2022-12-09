fcb7aed51b3cb2334cf4144175d6a52bb1c9236b
Aminekham <lkham0508@gmail.com>
2022-12-09 11:48:32 +0100

    exec

--- execute_the_line.c
+++ execute_the_line.c
@@ -4,16 +4,14 @@
  *@buuf: the buffer with the whole command
  *Return: integer of the status
  */
-int execute_the_line(char * buuf[])
+int execute_the_line(char *buuf[])
 {
 	int v = 0;
-	char c[] = "/bin/";
 
-	strcat(c, buuf[0]);
-	v = execve(c, buuf, NULL);
+	v = execve(buuf[0], buuf, NULL);
 	if (v == -1)
 	{
-		printf("command not found");
+		return (v);
 	}
 	return (v);
 }
